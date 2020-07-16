bf2255b05244a045cf70e9578a588860fba33164
Kyunghun Lee <Kyunghun.lee@nih.gov>
2020-07-16 16:18:14 -0400

    Documentation updated

--- DoorTask/driver.py
+++ DoorTask/driver.py
@@ -1,6 +1,4 @@
-import glob
-import re
-import random
+import glob,re,random,time
 import pandas as pd
 from psychopy import core, visual, event, sound
 from psychopy.hardware import joystick
@@ -14,6 +12,7 @@ params = {
     'nTrials': 5,            # number of trials in this session
     'subjectID' : 23986,      # Subject ID
     'DistanceStart' : 50,
+    'DistanceLockWaitTime' : 10, # Distance lock wait time.
     'winSize': 1,             # time when stimulus is presented (in seconds)
     'ISI': 2,                 # time between when one stimulus disappears and the next appears (in seconds)
     'tStartup': 2,            # pause time before starting first stimulus
@@ -71,29 +70,37 @@ Dict["Section"] = "VAS1"
 # VAS (Anxiety)
 Dict["VAS_type"] = "Anxiety"
 Dict["SessionStartDateTime"] = datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
-Dict["VAS_totalCoin"], Dict["VAS_RT"] = displayVAS(win,"How anxious do you feel right now?",['Not anxious', 'Very anxious'])
+startTime = time.time()
+Dict["VAS_totalCoin"], Dict["VAS_RT"] = displayVAS(win,"How anxious do you feel right now?",
+                                                   ['Not anxious', 'Very anxious'])
+Dict["VAS_RT"] = (time.time() - startTime) * 1000
 Df = tableWrite(Df,Dict) # Log the dict result on pandas dataFrame.
 
 # VAS (Avoidance)
 Dict["VAS_type"] = "Avoidance"
 Dict["SessionStartDateTime"] = datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
+startTime = time.time()
 Dict["VAS_totalCoin"], Dict["VAS_RT"] = displayVAS(win,"How much do you feel like taking part in the task?",
                                                ['Not at all', 'Very much'])
+Dict["VAS_RT"] = (time.time() - startTime) * 1000
 Df = tableWrite(Df,Dict) # Log the dict result on pandas dataFrame.
 
 # VAS (Tired)
 Dict["VAS_type"] = "Tired"
 Dict["SessionStartDateTime"] = datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
+startTime = time.time()
 Dict["VAS_totalCoin"], Dict["VAS_RT"] = displayVAS(win,"How tired are you right now?",['Not at all tired', 'Very tired'])
+Dict["VAS_RT"] = (time.time() - startTime) * 1000
 Df = tableWrite(Df,Dict) # Log the dict result on pandas dataFrame.
 
 # VAS (Mood)
 Dict["VAS_type"] = "Mood"
 Dict["SessionStartDateTime"] = datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
+startTime = time.time()
 Dict["VAS_totalCoin"], Dict["VAS_RT"] = displayVAS(win,"Think about your mood right now. How would you describe it?",
                        ['Worst mood ever', 'Best mood ever'])
+Dict["VAS_RT"] = (time.time() - startTime) * 1000
 Df = tableWrite(Df,Dict) # Log the dict result on pandas dataFrame.
-
 # ====================== #
 # ===== Instruction ==== #
 # ====================== #
@@ -138,19 +145,36 @@ for i in range(iterNum):
     img1 = visual.ImageStim(win=win, image=imgFile, units="pix", opacity=1, size=(width,height))
     img1.draw();win.flip();event.waitKeys()
 
+    startTime = time.time()
+    Dict["Distance_max"] = Dict["Distance_min"] = params["DistanceStart"]
     while(c[0]!="return"):
+        # If waittime is longer than 10 sec, exit this loop.
+        Dict["DoorAction_RT"] = (time.time() - startTime) * 1000
+        if Dict["DoorAction_RT"] / 1000 > params['DistanceLockWaitTime']:
+            continue
         core.wait(1 / 60)
         c = event.waitKeys()  # read a character
         if c[0] == "up" and level < 100:
             level += 1
         elif c[0] == "down" and level > 0:
             level -= 1
+        Dict["Distance_max"] = max(Dict["Distance_max"],level)
+        Dict["Distance_min"] = min(Dict["Distance_min"],level)
 
         width = params["screenSize"][0] * (1 - level / 110)
         height = params["screenSize"][1] * (1 - level / 110)
         img1 = visual.ImageStim(win=win, image=imgFile, units="pix", opacity=1, size=(width, height))
         img1.draw();win.flip()
+
+    if c[0] == "return":
+        Dict["Distance_lock"] = 1
+    else:
+        Dict["Distance_lock"] = 0
     Dict["DistanceFromDoor_SubTrial"] = level
+
+    # Door Anticipation time
+    Dict["Door_anticipation_time"] = random.uniform(0, 2) * 1000
+    time.sleep(Dict["Door_anticipation_time"] / 1000)
 
     if random.random() < level * 0.01:
         Dict["Door_opened"] = "closed"
@@ -176,7 +200,6 @@ for i in range(iterNum):
             sound1.stop()
             totalCoin -= int(p)
             displayText(win, "Lose your totalCoin: " + str(p) + "!!\n\n Total totalCoin: " + str(totalCoin))
-            event.waitKeys()
         else:
             Dict["Door_outcome"] = "reward"
             awardImg = "./img/outcomes/" + r + "_reward.jpg"
@@ -192,7 +215,9 @@ for i in range(iterNum):
             sound1.stop()
             totalCoin += int(r)
             displayText(win, "Earn your coin: " + str(r) + "!!\n\n Total Coin: " + str(totalCoin))
-            event.waitKeys()
+    startTime = time.time()
+    event.waitKeys()
+    Dict["ITI_duration"] = (time.time() - startTime) * 1000
     Dict["Total_coins"] = totalCoin
     Df = tableWrite(Df, Dict)  # Log the dict result on pandas dataFrame.
 
