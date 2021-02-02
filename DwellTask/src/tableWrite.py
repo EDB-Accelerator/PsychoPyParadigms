0ba0d198951554be618c491d8bf8b8e8b066ab53
Kyunghun Lee <kyunghun.lee@nih.gov>
2021-02-02 16:41:56 -0500

    doc updateD

--- DwellTask/src/tableWrite.py
+++ DwellTask/src/tableWrite.py
@@ -1,7 +1,33 @@
+# Python3-based package
+"""
+MIT License
+
+Copyright (c) 2021 NIMH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import pandas as pd
 import datetime
 
 def tableWriteRaw(Df, Dict):
+
     # Move data in Dict into Df.
     Df = Df.append(pd.Series(dtype=float), ignore_index=True)  # Insert Empty Rows
     Dict["TimeStamp"] =datetime.datetime.utcnow().strftime("%m%d%Y_%H:%M:%S.%f")[:-4]
@@ -9,10 +35,8 @@ def tableWriteRaw(Df, Dict):
         Df[key].loc[len(Df) - 1] = Dict[key]  # FYI, len(Df)-1: means the last row of pandas dataframe.
     return Df
 
-# Header = ["TimeStamp","expName","subjectID","Session","Run","Block","TrialCount","Section","Section Start Time",
-#           "Section End Time","Image Displayed","Button Pressed","Button Response Time"]
-
 def tableWrite(df,params,dict):
+
     # Move data in Dict into Df.
     df = df.append(pd.Series(dtype=float), ignore_index=True)  # Insert Empty Rows
     dict["Run"] = params["Run"]
