4a61f5da4ba6a07fc024fe50d20ecd46202906f4
Kyunghun Lee <Kyunghun.lee@nih.gov>
2021-06-01 22:19:39 -0400

    door task updated

--- DwellScanningEyeTrack/run.bat
+++
@@ -1,3 +0,0 @@
-@setlocal enableextensions
-@cd /d "%~dp0"
-"C:\Users\leek13\AppData\Local\PsychoPy3\python.exe" main.py
---
+++ DwellScanningEyeTrack/runUser.bat
@@ -0,0 +1,3 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+"%UserProfile%/AppData/Local/PsychoPy3/python.exe" main.py
