d138c354769dff94709d04521bf0cf8b404813ed
Kyunghun Lee <kyunghun.lee@nih.gov>
2021-07-27 21:15:24 -0400

    doc updated

--- AXCPT/main.py
+++ AXCPT/main.py
@@ -29,18 +29,23 @@ SOFTWARE.
 
 # Import dependencies.
 import sys
-from psychopy import visual,core
-import pandas as pd
-import glob
+from psychopy import visual,core,event
+import time
 
 # Import developer-defined functions
 sys.path.insert(1, './src')
 from PlayUserInputGUI import PlayUserInputGUI
 
+def waitAndGetUserInput(c,waitTime):
+    startTime = time.time()
+    while time.time()-startTime < waitTime:
+        if c == []:
+            c = event.getKeys()
+        core.wait(1/120)
 
+    return c
 
-
-
+# Receive User input from GUI window
 UserInputBank = PlayUserInputGUI()
 
 # Declare primary task parameters.
@@ -85,83 +90,120 @@ win.mouseVisible = False
 message = visual.TextStim(win,text="Welcome!\n\n"+
                           "Please remember the task where we ask you to\n"+
                           "remember a series of letters\n\n\n\n\n\n\n"+
-                          "Please press SPACE to see an example.",
+                          "Please press SPACE BAR to see an example.",
                                   units='pix', wrapWidth=1000, color="white",height=25)
 message.draw()
 win.flip()
 WaitUserSpace()
 
 # Introduction
-message = visual.TextStim(win,text="The task sequence looks like:",
-                                  units='pix', wrapWidth=1000, color="white",height=25,pos=[0,250])
-message.draw()
-win.flip()
-WaitUserSpace()
+c = ['Y']
+while(c[0].upper()=='Y'):
 
-for i in range(3):
-    imgFile = "resource/img/intro" + str(i) + ".JPG"
-    img1 = visual.ImageStim(win=win, image=imgFile, units="pix", opacity=1)
-    img1.draw()
+    message = visual.TextStim(win,text="The task sequence looks like:",
+                                      units='pix', wrapWidth=1000, color="white",height=25,pos=[0,250])
     message.draw()
     win.flip()
     WaitUserSpace()
 
-# Introduction Slide 2
-message = visual.TextStim(win,text="Press the YES (index) key as quickly as you can when you see the\n"+
-                          "blue letter that completes the target sequence. Press the NO\n"+
-                          "(middle) key as quickly as you can for all other letters.\n\n\n\n\n"+
-                          "Please Space Bar to continue",
-                                  units='pix', wrapWidth=1000, color="white",height=25)
-message.draw()
-win.flip()
-WaitUserSpace()
 
-# Introduction Slide 3
-message = visual.TextStim(win,text="Are you ready to start the task\n"
-                                   "or\n"
-                                   "should we review the instructions again?",
-                                  units='pix', wrapWidth=1000, color="white",height=25)
-message.draw()
-win.flip()
-WaitUserSpace()
+    for i in range(3):
+        imgFile = "resource/img/intro" + str(i) + ".JPG"
+        img1 = visual.ImageStim(win=win, image=imgFile, units="pix", opacity=1)
+        img1.draw()
+        message.draw()
+        win.flip()
+        WaitUserSpace()
 
+    # Introduction Slide 2
+    message = visual.TextStim(win,text="Press the YES (index) key as quickly as you can when you see the\n"+
+                              "blue letter that completes the target sequence. Press the NO\n"+
+                              "(middle) key as quickly as you can for all other letters.\n\n\n\n\n"+
+                              "Please SPACE BAR to continue",
+                                      units='pix', wrapWidth=1000, color="white",height=25)
+    message.draw()
+    win.flip()
+    WaitUserSpace()
 
-
-
-
+    # Introduction Slide 3
+    message = visual.TextStim(win,text="Are you ready to start the task\n"
+                                       "or\n"
+                                       "should we review the instructions again?\n\n\n"
+                                       "Press Y if Yes / Press N if No",
+                                      units='pix', wrapWidth=1000, color="white",height=25)
+    message.draw()
+    win.flip()
+    c = ['']
+    # Wait for user types "y" or "n".
+    while (c[0].upper() != "Y" and c[0].upper() != "N"):
+        core.wait(1 / 120)
+        c = event.waitKeys()  # read a characters
 
 
 timingFiles = glob.glob('timing/*.csv')
 timingFile = timingFiles[0]
-numTrial = int(params['numTrial'])
+# numTrial = int(params['numTrial'])
+numTrial = 3
 dfTiming = pd.read_csv(timingFile,header=None,names=['Trial Type','Delay Between Letters', 'Delay Between Trials'])
+
+# ITI
+message = visual.TextStim(win, text="+", wrapWidth=2,units='pix',color="white",height=50,pos=[-25,0])
+message.draw()
+win.flip()
+c = ['']
+# Wait for user types "5".
+while (c[0]!="5"):
+    core.wait(1 / 120)
+    c = event.waitKeys()  # read a character
+
+numTrial = 3
 
 for i in range(numTrial):
 
-    # ITI
-    message = visual.TextStim(win, text="+", wrapWidth=2,color="black")
+    # Get Timing Information
+    trialLetter = dfTiming.iloc[i]['Trial Type']
+    delayBetweenLetters = dfTiming.iloc[i]['Delay Between Letters'] / 1000
+    delayBetweenTrials = dfTiming.iloc[i]['Delay Between Trials'] / 1000
+
+    # Display the first letter (cue)
+    message = visual.TextStim(win, text=trialLetter[0],wrapWidth=2,units='pix',color="white",height=100,pos=[-25,0])
     message.draw()
     win.flip()
+    c = waitAndGetUserInput([],0.5)
 
-    core.wait(2)
-
-    # Get Letter type
-    trialLetter = dfTiming.iloc[i]['Trial ' \
-                                   'Type']
-
-    # Display the first letter
-    message = visual.TextStim(win, text=trialLetter[0],
-                              units='norm', wrapWidth=2, color="black")
+    # Display Fixation Cross (response window)
+    message = visual.TextStim(win, text="+", wrapWidth=2,units='pix',color="white",height=50,pos=[-25,0])
     message.draw()
     win.flip()
-    core.wait(0.5)
+    c = waitAndGetUserInput(c,1.5)
+    print(c)
 
-    # Display the second letter
-    message = visual.TextStim(win, text=trialLetter[1],
-                              units='norm', wrapWidth=2, color="black")
+    # Display Fixation Cross (Delay Between Letters)
+    message = visual.TextStim(win, text="+", wrapWidth=2,units='pix',color="white",height=50,pos=[-25,0])
     message.draw()
     win.flip()
-    core.wait(0.5)
+    core.wait(delayBetweenLetters)
+
+    # Display the second letter (probe)
+    message = visual.TextStim(win, text=trialLetter[1],wrapWidth=2,units='pix',color="white",height=100,pos=[-25,0])
+    message.draw()
+    win.flip()
+    c = waitAndGetUserInput([],0.5)
+
+    # Display Fixation Cross (response window)
+    message = visual.TextStim(win, text="+", wrapWidth=2,units='pix',color="white",height=50,pos=[-25,0])
+    message.draw()
+    win.flip()
+    c = waitAndGetUserInput(c,1.5)
+
+    # Display Fixation Cross (Delay Between Trials)
+    message = visual.TextStim(win, text="+", wrapWidth=2,units='pix',color="white",height=50,pos=[-25,0])
+    message.draw()
+    win.flip()
+    core.wait(delayBetweenTrials)
+    print(c)
+
+
 
 # message.draw()
 # win.flip()
@@ -169,3 +211,5 @@ for i in range(numTrial):
 # win.close()
 #
 #
+
+# c = waitAndGetUserInput(c,20)
