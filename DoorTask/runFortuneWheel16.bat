08642e0de9df355a647bfc06e9e0e04470d3de40
Kyunghun Lee <Kyunghun.lee@nih.gov>
2022-06-02 08:55:26 -0400

    door task updated

--- DoorTask/runFortuneWheel16.bat
+++ DoorTask/runFortuneWheel16.bat
@@ -1,4 +1,5 @@
 @echo off
+set arg1=%1
 echo Running the script...
 taskkill /F /IM chrome.exe /T > nul
-start /wait /min cmd /C "C:\Program Files\Google\Chrome\Application\chrome.exe" -kiosk -fullscreen --window-size=1024,768 --force-device-scale-factor=1 --app=C:\Users\leek13\Documents\GitHub\PsychoPyParadigms\DoorTask\FortuneWheel\index16.html
+start /wait /min cmd /C "C:\Program Files\Google\Chrome\Application\chrome.exe" -kiosk -fullscreen --window-size=1024,768 --force-device-scale-factor=1 --app=%arg1%\FortuneWheel\index16.html
--- DoorTask/src/FortuneGamePlay.py
+++ DoorTask/src/FortuneGamePlay.py
@@ -1,26 +1,29 @@
 import sys
 sys.path.insert(1, './src')
 
-import datetime
+import datetime,os
 import subprocess as subp
 from psychopy import visual
 from Helper import waitUserSpace, tableWrite
 
 def FortuneGamePlay(Df, win,params,SectionName,gameResult):
     Dict = {
+        "Subject": params['subjectID'],
+        "Session": params["Session"],
+        "Version": params["Version"],
         "Section": SectionName,
         "SessionStartDateTime": datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"),
     }
 
     win.close()
     if gameResult == 18:
-        subp.check_call("runFortuneWheel18.bat", shell=True)
+        subp.check_call("runFortuneWheel18.bat "+os.getcwd(), shell=True)
         win = visual.Window(params['screenSize'], monitor="testMonitor", color="black", winType='pyglet')
         message = visual.TextStim(win,
                                   text="You have won $18.\n\nPress the spacebar when you are ready to keep playing.",
                                   units='norm', wrapWidth=2)
     elif gameResult == 16:
-        subp.check_call("runFortuneWheel16.bat", shell=True)
+        subp.check_call("runFortuneWheel16.bat "+os.getcwd(), shell=True)
         win = visual.Window(params['screenSize'], monitor="testMonitor", color="black", winType='pyglet')
         message = visual.TextStim(win,
                                   text="You have won $16.\n\nPress the spacebar when you are ready to keep playing.",
