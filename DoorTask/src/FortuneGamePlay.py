d2e67fd7da66c38bab630cd79f872122bff47398
Kyunghun Lee <leekyh3@gmail.com>
2022-06-09 09:49:48 -0400

    door task updated (fortune wheel)

--- DoorTask/src/FortuneGamePlay.py
+++ DoorTask/src/FortuneGamePlay.py
@@ -5,6 +5,8 @@ import datetime,os
 import subprocess as subp
 from psychopy import visual
 from Helper import waitUserSpace, tableWrite
+from JoystickInput import JoystickInput
+import time
 
 def FortuneGamePlay(Df, win,params,SectionName,gameResult):
     Dict = {
@@ -15,21 +17,44 @@ def FortuneGamePlay(Df, win,params,SectionName,gameResult):
         "SessionStartDateTime": datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"),
     }
 
+    img1 = visual.ImageStim(win=win, image="./img/changeme.jpg", units="pix", opacity=1,size=(1024, 768))
+    img1.draw();win.flip()
+    win.mouseVisible = False
+    # waitUserSpace(Df, params)
+
+    # Wait for User input.
+    while (JoystickInput())['buttons_text'] == ' ':  # while presenting stimuli
+        time.sleep(0.001)
+        img1.draw();
+        win.flip()
+    while (JoystickInput())['buttons_text'] != ' ':  # while presenting stimuli
+        time.sleep(0.001)
+
+    win.mouseVisible = True
     win.close()
     if gameResult == 18:
         subp.check_call("runFortuneWheel18.bat "+os.getcwd(), shell=True)
         win = visual.Window(params['screenSize'], monitor="testMonitor", color="black", winType='pyglet')
         message = visual.TextStim(win,
-                                  text="You have won $18.\n\nPress the spacebar when you are ready to keep playing.",
+                                  text="You have won $18.\n\nClick when you are ready to keep playing.",
                                   units='norm', wrapWidth=2)
     elif gameResult == 16:
         subp.check_call("runFortuneWheel16.bat "+os.getcwd(), shell=True)
         win = visual.Window(params['screenSize'], monitor="testMonitor", color="black", winType='pyglet')
         message = visual.TextStim(win,
-                                  text="You have won $16.\n\nPress the spacebar when you are ready to keep playing.",
+                                  text="You have won $16.\n\nClick when you are ready to keep playing.",
                                   units='norm', wrapWidth=2)
     message.draw()
     win.flip()
-    waitUserSpace(Df, params)
+    win.mouseVisible = False
+    # waitUserSpace(Df, params)
+
+    # Wait for User input.
+    while (JoystickInput())['buttons_text'] == ' ':  # while presenting stimuli
+        time.sleep(0.001)
+        message.draw();
+        win.flip()
+    while (JoystickInput())['buttons_text'] != ' ':  # while presenting stimuli
+        time.sleep(0.001)
 
     return tableWrite(Df, params,Dict),win
