f0e36e71222f4f7f9f793e4f1805e6348cee5e91
Kyunghun Lee <Kyunghun.lee@nih.gov>
2022-07-18 13:59:17 -0400

    doc updated

--- SpatialNavigationTask/runvideo2.bat
+++ SpatialNavigationTask/runvideo2.bat
@@ -1,3 +1,4 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
-"C:\Program Files\VideoLAN\VLC\vlc.exe" --fullscreen --mouse-hide-timeout=0 --quiet --intf dummy --dummy-quiet --video-on-top --no-interact --no-sub-autodetect-file --no-video-title "./img/Version2/Video/video.mov" vlc://quit
+
+"%PROGRAMFILES%\VideoLAN\VLC\vlc.exe" --fullscreen --mouse-hide-timeout=0 --quiet --intf dummy --dummy-quiet --video-on-top --no-interact --no-sub-autodetect-file --no-video-title "./img/Version2/Video/video.mp4" vlc://quit
