8637c50f85fe5f60804f1f94534d3745c2bfe5e9
Kyunghun Lee <kyunghun.lee@nih.gov>
2024-08-12 09:01:13 -0400

    Doc updated

--- TAU2.0/main.py
+++ TAU2.0/main.py
@@ -1,5 +1,6 @@
 from psychopy import visual, event, core,gui
 import sys
+
 sys.path.insert(1,'src')
 
 # Function to display the input dialog and return the results
@@ -50,21 +51,70 @@ def display_check_scanner(win):
     gui.DlgFromDict(dictionary=info,title='Scanner Prepared?')
     # core.quit()  # the user hit cancel so exit
 
+user_info = get_user_input()
+params = {'sdan' : user_info['Subject Number|0'],
+          'session': user_info['Session Number|1'],
+          'version': user_info['Stimuli Set|2']
+}
+win = visual.Window(size=(1024, 768), fullscr=False, color=(0, 0, 0), colorSpace='rgb')
+
 display_text_and_wait_keys(win,'Instructions\n\n'
                 'In each trial, a + sign will appear in the center of the screen,\n'
                 'followed by a pair of faces, and then by a target: < or >\n\n'
                 'if the target is <, press the left button.\n'
                 'if the target is >, press the right button.\n\n'
                 'Respond as quickly as you can without making mistakes\n\n'
-                'Press any button to start.', ['space'])
-
-user_info = get_user_input()
+                'Press any button to start.', ['4','6'])
 
 # Create a window
-win = visual.Window(size=(1024, 768), fullscr=False, color=(0, 0, 0), colorSpace='rgb')
 key = display_text_and_wait_given_sec(win,"",1.0)
 display_check_scanner(win)
 display_text_and_wait_keys(win,'Waiting for the scanner..', ['4'])
+
+# TrialProc
+
+# 1. InitTrial
+# 'top box coordinates: anchor + jitter
+# Set probe_box_top = CSlideImage(Probe.States(Probe.ActiveState).Objects("top"))
+# probe_box_top.x = (PROBE_TOP_X_ANCHOR - JITTER_RANGE_HORIZONTAL/2) + CInt(random(0,JITTER_RANGE_HORIZONTAL))
+# probe_box_top.y = (PROBE_TOP_Y_ANCHOR - JITTER_RANGE_VERTICAL/2) + CInt(random(0,JITTER_RANGE_VERTICAL))
+#
+# 'bottom box coordinates: anchor + jitter
+# Set probe_box_bottom = CSlideImage(Probe.States(Probe.ActiveState).Objects("bottom"))
+# probe_box_bottom.x = (PROBE_BOTTOM_X_ANCHOR - JITTER_RANGE_HORIZONTAL/2) + CInt(random(0,JITTER_RANGE_HORIZONTAL))
+# probe_box_bottom.y = (PROBE_BOTTOM_Y_ANCHOR - JITTER_RANGE_VERTICAL/2) + CInt(random(0,JITTER_RANGE_VERTICAL))
+#
+# c.SetAttrib "horizontalJitterTop", PROBE_TOP_X_ANCHOR - probe_box_top.x
+# c.SetAttrib "VerticalJitterTop", PROBE_TOP_Y_ANCHOR - probe_box_top.y
+# c.SetAttrib "horizontalJitterBottom", PROBE_BOTTOM_X_ANCHOR - probe_box_bottom.x
+# c.SetAttrib "VerticalJitterBottom", PROBE_BOTTOM_Y_ANCHOR - probe_box_bottom.y
+#
+# trial_count = trial_count + 1
+###################
+
+# 2. Fixation (500ms)
+# 3. Faces
+# 4. Probe
+# 5. ITI
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Close the window and quit PsychoPy
 # win.close()
