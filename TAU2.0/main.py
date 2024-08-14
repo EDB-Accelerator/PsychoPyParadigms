8b0e8ed34d1ca3e59cfee70766a23e2be3e7b34c
Kyunghun Lee <kyunghun.lee@nih.gov>
2024-08-14 10:25:27 -0400

    doc updated

--- TAU2.0/main.py
+++ TAU2.0/main.py
@@ -1,8 +1,6 @@
 from psychopy import visual, event, core,gui
 import sys
 sys.path.insert(1,'src')
-from PlayUserInputGUI import PlayUserInputGUI
-
 from psychopy import prefs, gui
 import datetime
 
@@ -59,7 +57,7 @@ def display_text_and_wait_given_sec(win,text,wait_time):
         keys = event.getKeys()
         if keys:
             user_input = keys[0]
-            print(user_input)
+            # print(user_input)
             continue
     return user_input if user_input == '4' or user_input == '6' else None
 
@@ -67,6 +65,45 @@ def display_check_scanner(win):
     info = {}
     gui.DlgFromDict(dictionary=info,title='Scanner Prepared?')
     # core.quit()  # the user hit cancel so exit
+
+def display_faces_and_wait_given_sec(win, face_top, face_bottom, wait_time):
+    # Define the positions for the images
+    top_position = [0, 0.4]  # Adjust as needed
+    bottom_position = [0, -0.4]  # Adjust as needed
+
+    # Load and prepare the images
+    top_image = visual.ImageStim(win=win, image=face_top, pos=top_position)
+    bottom_image = visual.ImageStim(win=win, image=face_bottom, pos=bottom_position)
+
+    # Draw the images on the window
+    top_image.draw()
+    bottom_image.draw()
+
+    # Flip the window (i.e., display the stimulus)
+    win.flip()
+
+    # Display the text and check for a key press within the given time
+    timer = core.Clock()
+    response = None
+    response_time = None
+
+    while timer.getTime() < wait_time:
+        # Check for key press
+        keys = event.getKeys(timeStamped=timer)
+        if keys:
+            response, response_time = keys[0]
+            if response in ['4', '6']:
+                print(response)  # Optional: Print the response for debugging
+                break  # Exit the loop after recording the first valid response
+
+    # Pause for the remaining time if a response was received before wait_time
+    remaining_time = wait_time - timer.getTime()
+    if remaining_time > 0:
+        core.wait(remaining_time)
+
+    # Return the recorded response and response time only if it is valid
+    return (response, response_time) if response in ['4', '6'] else (None, None)
+
 
 # Initialization
 # Constants
@@ -141,139 +178,142 @@ def load_and_concat_csv_files(prefixes, categories, directory='timing'):
 prefixes = ['f', 'm']
 categories = ['NTc', 'NN', 'NTi']
 
-# Load and concatenate all files into a single DataFrame
-df_all = load_and_concat_csv_files(prefixes, categories)
 
-# Load ITI files
-df_iti = pd.read_csv("timing/ITIList.csv")
-ITIs = list(df_iti['ITIDur']) * 6
-random.shuffle(ITIs)
 
-# Shuffle the combined DataFrame
-df_all = df_all.sample(frac=1).reset_index(drop=True)
-
-def display_faces_and_wait_given_sec(win, face_top, face_bottom, wait_time):
-    # Define the positions for the images
-    top_position = [0, 0.4]  # Adjust as needed
-    bottom_position = [0, -0.4]  # Adjust as needed
-
-    # Load and prepare the images
-    top_image = visual.ImageStim(win=win, image=face_top, pos=top_position)
-    bottom_image = visual.ImageStim(win=win, image=face_bottom, pos=bottom_position)
-
-    # Draw the images on the window
-    top_image.draw()
-    bottom_image.draw()
-
-    # Flip the window (i.e., display the stimulus)
-    win.flip()
-
-    # Display the text and check for a key press within the given time
-    timer = core.Clock()
-    response = None
-    response_time = None
-    while timer.getTime() < wait_time:
-        if response == '4' and response == '6':
-            continue
-        keys = event.getKeys(timeStamped=timer)
-        if keys:
-            response, response_time = keys[0]
-            print(response)
-            continue
-    return response, response_time if response == '4' or response == '6' else None
 
 # Initialize a list to hold trial data
 trial_data = []
 
-# for i in range(len(df_all)):
-for i in range(10):
-    trial_id = i + 1
-    df = df_all.iloc[i]
+for list_idx in range(2):
 
-    # 1. Fixation Cross
+    # Load and concatenate all files into a single DataFrame
+    df_all = load_and_concat_csv_files(prefixes, categories)
+
+    # Load ITI files
+    df_iti = pd.read_csv("timing/ITIList.csv")
+    ITIs = list(df_iti['ITIDur']) * 6
+    random.shuffle(ITIs)
+
+    # Shuffle the combined DataFrame
+    df_all = df_all.sample(frac=1).reset_index(drop=True)
+
+    for i in range(len(df_all)):
+    # for i in range(10):
+        trial_id = i + 1
+        df = df_all.iloc[i]
+
+        # 1. Fixation Cross
+        start_time = core.Clock()
+        display_text_and_wait_given_sec(win, "+", 0.5)
+        fixation_duration = start_time.getTime()
+        trial_data.append({
+            'Trial_ID': trial_id,
+            'Step': 'Fixation',
+            'Stimulus': '+',
+            'Response': None,
+            'ResponseTime': None,
+            'Duration': fixation_duration,
+            'CorrectAnswer': None,
+            'Type': None
+        })
+
+        # 2. Display Faces
+        start_time = core.Clock()
+        FaceTop = f"enlarged_images/{params['version']}/{df['FaceTop']}.bmp"
+        FaceBottom = f"enlarged_images/{params['version']}/{df['FaceBottom']}.bmp"
+
+        # top_image = visual.ImageStim(win=win, image=FaceTop, pos=[0, 0.4])
+        # bottom_image = visual.ImageStim(win=win, image=FaceBottom, pos=[0,-0.4])
+        # top_image.draw()
+        # bottom_image.draw()
+        # win.flip()
+        display_faces_and_wait_given_sec(win, FaceTop, FaceBottom, 0.5)
+        face_display_duration = start_time.getTime()
+        trial_data.append({
+            'Trial_ID': trial_id,
+            'Step': 'Display Faces',
+            'Stimulus': f'{FaceTop} / {FaceBottom}',
+            'Response': "",
+            'ResponseTime': "",
+            'Duration': face_display_duration,
+            'CorrectAnswer': df['CorrectResponse'][0] if pd.notna(df['CorrectResponse'][0]) else None,
+            'Type': df['type']
+        })
+
+        # Probe
+        start_time = core.Clock()
+        response,response_time = display_faces_and_wait_given_sec(win, FaceTop, FaceBottom, 1)
+        face_display_duration = start_time.getTime()
+        trial_data.append({
+            'Trial_ID': trial_id,
+            'Step': 'Display Faces',
+            'Stimulus': f'{FaceTop} / {FaceBottom}',
+            'Response': "",
+            'ResponseTime': "",
+            'Duration': face_display_duration,
+            'CorrectAnswer': df['CorrectResponse'][0] if pd.notna(df['CorrectResponse'][0]) else None,
+            'Type': df['type']
+        })
+
+        # ITI
+        start_time = core.Clock()
+        display_text_and_wait_given_sec(win,"",ITIs[i]/1000)
+        iti_duration = start_time.getTime()
+        trial_data.append({
+            'Trial_ID': trial_id,
+            'Step': 'ITI',
+            'Stimulus': 'ITI',
+            'Response': None,
+            'ResponseTime': None,
+            'Duration': iti_duration,
+            'CorrectAnswer': None,
+            'Type': None
+        })
+
+        # Fixation
+        start_time = core.Clock()
+        display_text_and_wait_given_sec(win, "+", 2.5)
+        fixation_duration = start_time.getTime()
+        trial_data.append({
+            'Trial_ID': trial_id,
+            'Step': 'Fixation',
+            'Stimulus': '+',
+            'Response': None,
+            'ResponseTime': None,
+            'Duration': fixation_duration,
+            'CorrectAnswer': None,
+            'Type': None
+        })
+
+    # Rest
     start_time = core.Clock()
-    display_text_and_wait_given_sec(win, "+", 0.5)
-    fixation_duration = start_time.getTime()
+    display_text_and_wait_keys(win, 'Please rest', ['4', '6'])
+    rest_duration = start_time.getTime()
     trial_data.append({
-        'Trial_ID': trial_id,
-        'Step': 'Fixation',
-        'Stimulus': '+',
+        'Trial_ID': None,
+        'Step': 'REST',
+        'Stimulus': 'Please Rest',
         'Response': None,
         'ResponseTime': None,
-        'Duration': fixation_duration,
+        'Duration': rest_duration,
         'CorrectAnswer': None,
         'Type': None
     })
 
-    # 2. Display Faces
-    start_time = core.Clock()
-    FaceTop = f"enlarged_images/{params['version']}/{df['FaceTop']}.bmp"
-    FaceBottom = f"enlarged_images/{params['version']}/{df['FaceBottom']}.bmp"
-
-    # top_image = visual.ImageStim(win=win, image=FaceTop, pos=[0, 0.4])
-    # bottom_image = visual.ImageStim(win=win, image=FaceBottom, pos=[0,-0.4])
-    # top_image.draw()
-    # bottom_image.draw()
-    # win.flip()
-    display_faces_and_wait_given_sec(win, FaceTop, FaceBottom, 0.5)
-    face_display_duration = start_time.getTime()
-    trial_data.append({
-        'Trial_ID': trial_id,
-        'Step': 'Display Faces',
-        'Stimulus': f'{FaceTop} / {FaceBottom}',
-        'Response': "",
-        'ResponseTime': "",
-        'Duration': face_display_duration,
-        'CorrectAnswer': df['CorrectResponse'][0] if pd.notna(df['CorrectResponse'][0]) else None,
-        'Type': df['type']
-    })
-
-    # Probe
-    start_time = core.Clock()
-    response,response_time = display_faces_and_wait_given_sec(win, FaceTop, FaceBottom, 1)
-    face_display_duration = start_time.getTime()
-    trial_data.append({
-        'Trial_ID': trial_id,
-        'Step': 'Display Faces',
-        'Stimulus': f'{FaceTop} / {FaceBottom}',
-        'Response': "",
-        'ResponseTime': "",
-        'Duration': face_display_duration,
-        'CorrectAnswer': df['CorrectResponse'][0] if pd.notna(df['CorrectResponse'][0]) else None,
-        'Type': df['type']
-    })
-
-    # ITI
-    start_time = core.Clock()
-    display_text_and_wait_given_sec(win,"",ITIs[i]/1000)
-    iti_duration = start_time.getTime()
-    trial_data.append({
-        'Trial_ID': trial_id,
-        'Step': 'ITI',
-        'Stimulus': 'ITI',
-        'Response': None,
-        'ResponseTime': None,
-        'Duration': iti_duration,
-        'CorrectAnswer': None,
-        'Type': None
-    })
-
-    # Fixation
-    start_time = core.Clock()
-    display_text_and_wait_given_sec(win, "+", 2.5)
-    fixation_duration = start_time.getTime()
-    trial_data.append({
-        'Trial_ID': trial_id,
-        'Step': 'Fixation',
-        'Stimulus': '+',
-        'Response': None,
-        'ResponseTime': None,
-        'Duration': fixation_duration,
-        'CorrectAnswer': None,
-        'Type': None
-    })
+# Convert the list of trial data to a DataFrame
+df_trials = pd.DataFrame(trial_data)
 
 
+from datetime import datetime
+
+# Get the current date and time
+now = datetime.now()
+formatted_datetime = now.strftime("%Y%m%d_%H%M%S")
+
+# Save the DataFrame to a CSV file with date and time in the filename
+df_trials.to_csv(f'results/subject_{params["sdan"]}_session_{params["session"]}_{formatted_datetime}.csv', index=False)
+
+display_text_and_wait_given_sec(win,"Thank you!",1.0)
 
 # df_f = {
 #     'NTc': pd.read_csv(f'timing/fNTc.csv'),
